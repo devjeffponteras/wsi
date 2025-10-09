742c394b58de682659f732bdda19a3e8eeb32d04
wsiarcalaalvin <arcala.alvin@webfocus.ph>
2025-10-09 14:18:33 +0800

    changes for sitemap

--- routes/web.php
+++ routes/web.php
@@ -81,7 +81,7 @@ Route::get('/phpinfo', function () {
 
     // Sitemap
         Route::get('/sitemap', [FrontController::class, 'sitemap'])->name('sitemap');
-        // Route::get('/sitemap', [SitemapController::class, 'index'])->name('sitemap');
+        Route::get('/xml', [SitemapController::class, 'xml']);
     //
 
     // Portfolio
